de143daad289a94de1c1af49096ac24623f64a72
Mateusz Szewc <mateuszszewc@op.pl>
2023-03-09 23:57:22 +0100

    feat: ui starter project

---
+++ lego-store-ui/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import LoginPage from './pages/LoginPage.svelte';
+  import RegisterPage from './pages/RegisterPage.svelte';
+</script>
+
+<svelte:head>
+	<title>Lego Store</title>
+</svelte:head>
+
+<main>
+  <!-- <LoginPage/> -->
+  <RegisterPage/>
+</main>
+
+<style>
+</style>
---
+++ lego-store-ui/src/pages/RegisterPage.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Button, Card, Input, Label } from "flowbite-svelte";
+</script>
+
+<main>
+  <div class="login-form">
+    <Card>
+      <h4 class="text-2xl font-bold mb-6 text-center">Rejestracja</h4>
+      <div class="flex flex-col ">
+        <div class="mb-6">
+          <Label for="default-input" class="block mb-2">Email</Label>
+          <Input id="default-input" placeholder="email" />
+        </div>
+        <div class="mb-6">
+          <Label for="default-input" class="block mb-2">Hasło</Label>
+          <Input id="default-input" placeholder="password" type="password" />
+        </div>
+        <div class="mb-6">
+          <Label for="default-input" class="block mb-2">Powtórz hasło</Label>
+          <Input id="default-input" placeholder="password" type="password" />
+        </div>
+        <Button class="text-center w-2/3 self-center" color="dark">Zarejestruj</Button>
+      </div>
+    </Card>
+  </div>
+</main>
+
+<style>
+  .login-form {
+    min-width: 400px;
+  }
+</style>
