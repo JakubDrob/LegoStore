e0260a0e2acfa3c7af4e3efc658e9091aa58f614
Szewc Mateusz <mateusz.szewc@bffgroup.com>
2023-04-03 19:43:05 +0200

    feat: Added CORS support

--- Backend/app.py
+++ Backend/app.py
@@ -3,8 +3,10 @@ from flask_restful import Api
 from resources.user_resource import User
 from resources.product_resource import Product
 from resources.set_resource import Set
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(User, '/users', '/users/<string:user_id>')
