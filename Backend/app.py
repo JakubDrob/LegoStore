31eaeebb7a33d91107d1acd5cc8583c34794eed8
PiotrWesoly <Piotrpwesoly@gmail.com>
2023-04-06 19:33:21 +0200

    fix issue with static image download

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_restful import Api
 from resources.user_resource import User
 from resources.product_resource import Product
@@ -12,6 +12,10 @@ api = Api(app)
 api.add_resource(User, '/users', '/users/<string:user_id>')
 api.add_resource(Product, '/products')
 api.add_resource(Set, '/sets')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == "__main__":
     app.run(debug=True)
