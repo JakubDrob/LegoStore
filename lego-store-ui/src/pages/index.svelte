1260fd36d837e5a9f5f81f89ac8cfeba277fdd4f
PiotrWesoly <Piotrpwesoly@gmail.com>
2023-05-21 11:18:37 +0200

    Fist implementation of carousel, weird flashing images

--- lego-store-ui/src/pages/index.svelte
+++ lego-store-ui/src/pages/index.svelte
@@ -1,3 +1,44 @@
-<script lang="ts">
+<script>
+    // import Carousel from '../components/Carousel.svelte';
+    import { Carousel } from 'flowbite-svelte'
+    import { Card } from "flowbite-svelte";
+    import { CarouselTransition } from 'flowbite-svelte'
+    import { bounceInOut, backInOut, cubicInOut } from 'svelte/easing';
+  
+    let showThumbs = false;
+    let showCaptions = false;
+    let border = true;
 
-</script>
+    const images = [
+        {id: 0,
+        name: "Cosmic timetraveler",
+        imgurl: "1.webp",
+        attribution: "cosmic-timetraveler-pYyOZ8q7AII-unsplash.com",
+        },
+        {id: 1,
+        name: "Cosmic timetraveler",
+        imgurl: "3.webp",
+        attribution: "cosmic-timetraveler-pYyOZ8q7AII-unsplash.com",
+        },
+    ];
+
+  </script>
+  
+  <main class="flex justify-center w-screen page-body">
+    <Card size = "xl" vertical class="h-96 w-full mt-10 bg-opacity-60 bg-black">
+        <div class="flex justify-center">
+            <h2 class="text-2xl text-white font-bold">Nowości !</h2>
+        </div>
+        <CarouselTransition {images} transitionType="slide" transitionParams="{{duration: 1000, easing: cubicInOut}}" showCaptions={false} showThumbs={false}/>
+        <!-- <Carousel {images} {border} loop {showThumbs} {showCaptions} duration="3000"/> -->
+      </Card>
+  </main>
+  
+
+  <style>
+    .page-body {
+      background-image:  url('../assets/background.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
